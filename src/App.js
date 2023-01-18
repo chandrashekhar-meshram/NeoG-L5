91adf63d1c17c8b4d537a3707d4c7bc28c9c3755
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-19 00:27:15 +0530

    P3 - welcome massage - onChange

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
 export default function App() {
- 
+  const [userName, setUsernName] = useState("");
   const color = "green";
-
+  
+  const userNameHandler = (e) => {
+    setUsernName(e.target.value);
+    console.log(userName)
+  }
   return (
     <div>
       <h1>Hello <span style={{color:color}}> StackBlitz!</span></h1>
-      <p>Start editing to see some magic happen :)</p>
+      <input 
+        style={{padding: "0.5rem"}} 
+        placeholder="Write user name"
+        onChange={userNameHandler}
+      />
+      <h1>Welcome {userName}</h1>
     </div>
   );
 }
