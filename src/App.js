f1066ba75ef8262a6baa13f077f3be3339bc4ba6
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-19 00:55:20 +0530

    Like button example done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, {useState} from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
-  const [userName, setUsernName] = useState("");
-  const color = "green";
-  
-  const userNameHandler = (e) => {
-    setUsernName(e.target.value);
-    console.log(userName)
-  }
+  const [userName, setUsernName] = useState('');
+  const color = 'green';
+
+  const likeHandlerBtn = () => {
+    console.log('like button clicked');
+  };
   return (
     <div>
-      <h1>Hello <span style={{color:color}}> StackBlitz!</span></h1>
-      <input 
-        style={{padding: "0.5rem"}} 
-        placeholder="Write user name"
-        onChange={userNameHandler}
-      />
+      <h1>
+        Hello <span style={{ color: color }}> StackBlitz!</span>
+      </h1>
+      <button onClick={likeHandlerBtn}>Like</button>
       <h1>Welcome {userName}</h1>
     </div>
   );
