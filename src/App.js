512003872393c4688382a6b77b1b76731c1610d1
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-19 00:05:18 +0530

    P1 - inline style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Hello <span style={{color:"red"}}> StackBlitz!</span></h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
