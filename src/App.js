822060ed510abaced39a04758ff3d39a04770760
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-19 00:08:33 +0530

    P2 - color using variable

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
-
+ 
+  const color = "green";
 
   return (
     <div>
-      <h1>Hello <span style={{color:"red"}}> StackBlitz!</span></h1>
+      <h1>Hello <span style={{color:color}}> StackBlitz!</span></h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
