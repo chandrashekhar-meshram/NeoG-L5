699204a8ce8645a90cbd9331e9a72b0a618025ec
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-19 02:05:05 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
 import React, { useState } from 'react';
 import './style.css';
 
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance" 
+};
+
+const emojis = Object.keys(emojiDictionary);
+
 export default function App() {
-  const [userName, setUsernName] = useState('');
+  const [emoji, setEmoji] = useState('');
+  const [meaning, setMeaning] = useState("Translation will appear here.....");
   const color = 'green';
 
-  const likeHandlerBtn = () => {
-    console.log('like button clicked');
+  const changeHandler = (e) => {
+    const inputEmoji = e.target.value;
+    setEmoji(inputEmoji);
+
+    if(inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("Failed! to recognise this emoji");
+    }
+  }
+
+  const emojiClickHandler = (item) => {
+    setMeaning(emojiDictionary[item]); //item = inputEmoji
   };
+
   return (
     <div>
       <h1>
-        Hello <span style={{ color: color }}> StackBlitz!</span>
+        <span style={{ color: color, textDecoration:"underline" }}> Emogi 😊</span> Interpreter App
       </h1>
-      <button onClick={likeHandlerBtn}>Like</button>
-      <h1>Welcome {userName}</h1>
+      <div>
+        <input 
+          style={{padding:"1rem",minWidth:"70%", marginBottom:"0.5rem"}} 
+          value={emoji}
+          onChange={changeHandler}
+          placeholder={"put an emoji here to know the meaning"}
+        />
+      </div>
+      
+      <h2 style={{textAlign:"center"}}> {emoji} </h2>
+      <h3 style={{textAlign:"center"}}> {meaning} </h3>
+
+      {
+        emojis.map((item) => (
+          <span
+            style={{fontSize:'2rem', padding:"0.7rem", cursor:"pointer"}}
+            onClick={()=>emojiClickHandler(item)} 
+          >
+            {item}
+          </span>
+        ))
+      }
     </div>
   );
 }
